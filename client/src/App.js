e0fecbf286fed5c5dc5f6065847886083eb25899
parthndeshpande <parthndeshpande@gmail.com>
2023-06-16 16:12:26 +0530

    Base project commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRUD OPERATION</h1>
+
+      <div>
+        <input type='text' id='moviename'/>
+      </div>
+      <br/>
+      <div>
+        <input type='text' id='review'/>
+      </div>
+    <br/>
+      <button>SUBMIT</button>
+    </div>
+  );
+}
+
+export default App;
